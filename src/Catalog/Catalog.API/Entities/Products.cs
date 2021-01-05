cc694dc5f4828e72556b006fc42f6ea96a136d22
Tuan LM <deadlove011011@gmail.com>
2021-01-06 03:13:50 +0700

    update config

---
+++ src/Catalog/Catalog.API/Entities/Products.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Entities
+{
+    public class Products
+    {
+        public string id { get; set; }
+
+    }
+}
